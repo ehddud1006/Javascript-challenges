e15eafaba1b4af39f011c7f75906e77f9830a40d
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-10-13 20:44:27 +0900

    feat: 맞춰야하는 수 랜덤으로 생성하기

--- ehddud1006/javascript_baseball/src/index.js
+++ ehddud1006/javascript_baseball/src/index.js
@@ -1,6 +1,18 @@
+const randomNumberGenerator = () => {
+  const randomNumberList = [];
+  while (randomNumberList.length < 3) {
+    // eslint-disable-next-line no-undef
+    const N = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!randomNumberList.includes(N)) {
+      randomNumberList.push(N);
+    }
+  }
+  return randomNumberList;
+};
 export default function BaseballGame() {
   const submitButton = document.querySelector('#submit');
   const userInput = document.querySelector('#user-input');
+  const randomNumberList = randomNumberGenerator();
 
   const gameStart = (e) => {
     e.preventDefault();
