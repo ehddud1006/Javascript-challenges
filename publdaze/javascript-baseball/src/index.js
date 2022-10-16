b0b5997f6441c8816f1cdf79d561ca1ee21932cd
publdaze <publdaze@naver.com>
2022-10-16 18:05:12 +0900

    feat: 잘못된 값을 입력 시 입력값 비워주기

--- publdaze/javascript-baseball/src/index.js
+++ publdaze/javascript-baseball/src/index.js
@@ -7,21 +7,24 @@ import {
 } from './modules/validation.js';
 import alertMsg from './modules/announce.js';
 
-const alertOccuredErrorCase = inputNumArray => {
-  switch (false) {
-    case isNumberArray(inputNumArray):
-      alertMsg('입력값은 숫자로만 구성되어야합니다.');
-      break;
-    case isInRangeArray(inputNumArray, 1, 9):
-      alertMsg('1~9 사이의 값을 넣어주세요.');
-      break;
-    case isLengthEquals(inputNumArray, 3):
-      alertMsg('3개의 숫자를 넣어주세요.');
-      break;
-    case isEachUniqueNumber(inputNumArray):
-      alertMsg('중복된 값이 있으면 안 됩니다.');
-      break;
+const isOccuredError = inputNumArray => {
+  if (isNumberArray(inputNumArray)) {
+    alertMsg('입력값은 숫자로만 구성되어야합니다.');
+    return true;
   }
+  if (isInRangeArray(inputNumArray, 1, 9)) {
+    alertMsg('1~9 사이의 값을 넣어주세요.');
+    return true;
+  }
+  if (isLengthEquals(inputNumArray, 3)) {
+    alertMsg('3개의 숫자를 넣어주세요.');
+    return true;
+  }
+  if (isEachUniqueNumber(inputNumArray)) {
+    alertMsg('중복된 값이 있으면 안 됩니다.');
+    return true;
+  }
+  return false;
 };
 
 export default function BaseballGame() {
@@ -32,7 +35,7 @@ export default function BaseballGame() {
 
     const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     const inputNumArray = userInput.value.split('').map(Number);
-    alertOccuredErrorCase(inputNumArray);
+    if (isOccuredError(inputNumArray)) userInput.value = '';
 
     result.textContent = '';
   });
