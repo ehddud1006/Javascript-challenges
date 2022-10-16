e2d4b0fb1be45416ed2c08528a1871fc92d5c550
publdaze <publdaze@naver.com>
2022-10-16 19:27:32 +0900

    feat: 게임 재시작 버튼 클릭 시 초기화하기

--- publdaze/javascript-baseball/src/index.js
+++ publdaze/javascript-baseball/src/index.js
@@ -11,50 +11,58 @@ import {
   countMatchNumbersWithPos,
 } from './modules/counting.js';
 
-const isOccuredError = inputNumArray => {
-  if (!isNumberArray(inputNumArray)) {
-    alertMsg('입력값은 숫자로만 구성되어야합니다.');
-    return true;
-  }
-  if (!isInRangeArray(inputNumArray, 1, 9)) {
-    alertMsg('1~9 사이의 값을 넣어주세요.');
-    return true;
-  }
-  if (!isLengthEquals(inputNumArray, 3)) {
-    alertMsg('3개의 숫자를 넣어주세요.');
-    return true;
-  }
-  if (!isEachUniqueNumber(inputNumArray)) {
-    alertMsg('중복된 값이 있으면 안 됩니다.');
-    return true;
-  }
-  return false;
-};
-
-const isWin = strike => {
-  return strike === 3;
-};
-
-const getOutputText = (strike, ball) => {
-  let output = '';
-  if (ball > 0) output += ball + '볼 ';
-  if (strike > 0) output += strike + '스트라이크';
-  if (output.length === 0) output = '낫싱';
-
-  return output;
-};
-
 export default function BaseballGame() {
   const { userInput, submit, gameRestartButton, result } = getElements();
-  let randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
 
-  gameRestartButton.style.display = 'none';
+  let randomNumbers;
+
+  const init = () => {
+    randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    gameRestartButton.style.display = 'none';
+    userInput.value = '';
+    result.innerHTML = '';
+  };
+
+  const isOccuredError = inputNumArray => {
+    if (!isNumberArray(inputNumArray)) {
+      alertMsg('입력값은 숫자로만 구성되어야합니다.');
+      return true;
+    }
+    if (!isInRangeArray(inputNumArray, 1, 9)) {
+      alertMsg('1~9 사이의 값을 넣어주세요.');
+      return true;
+    }
+    if (!isLengthEquals(inputNumArray, 3)) {
+      alertMsg('3개의 숫자를 넣어주세요.');
+      return true;
+    }
+    if (!isEachUniqueNumber(inputNumArray)) {
+      alertMsg('중복된 값이 있으면 안 됩니다.');
+      return true;
+    }
+    return false;
+  };
+
+  const isWin = strike => {
+    return strike === 3;
+  };
+
+  const getOutputText = (strike, ball) => {
+    let output = '';
+    if (ball > 0) output += ball + '볼 ';
+    if (strike > 0) output += strike + '스트라이크';
+    if (output.length === 0) output = '낫싱';
+
+    return output;
+  };
+
+  init();
 
   submit.addEventListener('click', function (e) {
     e.preventDefault();
 
     const inputNumArray = userInput.value.split('').map(Number);
-    console.log(inputNumArray, randomNumbers);
+
     if (isOccuredError(inputNumArray)) userInput.value = '';
     else {
       const strike = countMatchNumbersWithPos(randomNumbers, inputNumArray);
@@ -63,14 +71,15 @@ export default function BaseballGame() {
         result.innerHTML =
           '🎉정답을 맞추셨습니다🎉<br/>게임을 새로 시작하시겠습니까?';
         gameRestartButton.style.display = 'block';
+
+        gameRestartButton.addEventListener('click', function (e) {
+          e.preventDefault();
+          init();
+        });
       } else {
-        result.innerHTML = getOutputText();
+        result.innerHTML = getOutputText(strike, ball);
       }
     }
-  });
-  gameRestartButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    // reset
   });
 }
 
