286beceab4b5bef2589c3c24c1110a9ce44e117a
publdaze <publdaze@naver.com>
2022-10-16 16:01:12 +0900

    feat: 1~9 사이의 수인지 검증하는 로직 구현

--- publdaze/javascript-baseball/src/modules/validation.js
+++ publdaze/javascript-baseball/src/modules/validation.js
@@ -4,3 +4,14 @@ const isNumberArray = arr => {
   });
   return true;
 };
+
+const isInRange = (x, min, max) => {
+  return x >= min && x <= max;
+};
+
+const isInRangeArray = (arr, min, max) => {
+  arr.forEach(element => {
+    if (!isInRange(element, min, max)) return false;
+  });
+  return true;
+};
