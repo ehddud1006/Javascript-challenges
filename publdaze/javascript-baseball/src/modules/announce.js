9f511a549e13c028a1d71c83edbbf0ffe34ca3ff
publdaze <publdaze@naver.com>
2022-10-16 23:06:24 +0900

    refactor: 예외 enum 처리, 함수 밖으로 빼기

--- publdaze/javascript-baseball/src/modules/announce.js
+++ publdaze/javascript-baseball/src/modules/announce.js
@@ -2,4 +2,13 @@ const alertMsg = msg => {
   alert(msg);
 };
 
-export default alertMsg;
+const getOutputText = (strike, ball) => {
+  let output = '';
+  if (ball > 0) output += ball + '볼 ';
+  if (strike > 0) output += strike + '스트라이크';
+  if (output.length === 0) output = '낫싱';
+
+  return output;
+};
+
+export { alertMsg, getOutputText };
---
+++ publdaze/javascript-baseball/src/modules/exception.js
@@ -0,0 +1,33 @@
+import {
+  isNumberArray,
+  isInRangeArray,
+  isLengthEquals,
+  isEachUniqueNumber,
+} from './validation.js';
+
+const ExceptionCase = {
+  NUMBER_FORMAT_EXCEPTION: Symbol('입력값은 숫자로만 구성되어야합니다.'),
+  RANGE_EXCEPTION: Symbol('1~9 사이의 값을 넣어주세요.'),
+  LENGTH_EXCEPTION: Symbol('3개의 숫자를 넣어주세요.'),
+  DUPLICATION_EXCEPTION: Symbol('중복된 값이 있으면 안됩니다.'),
+  NOTHING: Symbol(''),
+};
+Object.freeze(ExceptionCase);
+
+const checkOccuredError = inputNumArray => {
+  if (!isNumberArray(inputNumArray)) {
+    return ExceptionCase.NUMBER_FORMAT_EXCEPTION;
+  }
+  if (!isInRangeArray(inputNumArray, 1, 9)) {
+    return ExceptionCase.RANGE_EXCEPTION;
+  }
+  if (!isLengthEquals(inputNumArray, 3)) {
+    return ExceptionCase.LENGTH_EXCEPTION;
+  }
+  if (!isEachUniqueNumber(inputNumArray)) {
+    return ExceptionCase.DUPLICATION_EXCEPTION;
+  }
+  return ExceptionCase.NOTHING;
+};
+
+export { ExceptionCase, checkOccuredError };
