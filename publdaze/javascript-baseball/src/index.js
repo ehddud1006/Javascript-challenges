1b7916c45761dd027d20eab333abf21c737f1dc2
publdaze <publdaze@naver.com>
2022-10-16 19:05:58 +0900

    feat: 3스트라이크의 경우 처리

--- publdaze/javascript-baseball/src/index.js
+++ publdaze/javascript-baseball/src/index.js
@@ -31,28 +31,41 @@ const isOccuredError = inputNumArray => {
   return false;
 };
 
+const isWin = strike => {
+  return strike === 3;
+};
+
 const getOutputText = (strike, ball) => {
   let output = '';
   if (ball > 0) output += ball + '볼 ';
   if (strike > 0) output += strike + '스트라이크';
   if (output.length === 0) output = '낫싱';
+
   return output;
 };
 
 export default function BaseballGame() {
   const { userInput, submit, gameRestartButton, result } = getElements();
+  let randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+
+  gameRestartButton.style.display = 'none';
 
   submit.addEventListener('click', function (e) {
     e.preventDefault();
 
-    const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     const inputNumArray = userInput.value.split('').map(Number);
     console.log(inputNumArray, randomNumbers);
     if (isOccuredError(inputNumArray)) userInput.value = '';
     else {
       const strike = countMatchNumbersWithPos(randomNumbers, inputNumArray);
       const ball = countMatchNumbers(randomNumbers, inputNumArray) - strike;
-      result.textContent = getOutputText(strike, ball);
+      if (isWin(strike, ball)) {
+        result.innerHTML =
+          '🎉정답을 맞추셨습니다🎉<br/>게임을 새로 시작하시겠습니까?';
+        gameRestartButton.style.display = 'block';
+      } else {
+        result.innerHTML = getOutputText();
+      }
     }
   });
   gameRestartButton.addEventListener('click', function (e) {
