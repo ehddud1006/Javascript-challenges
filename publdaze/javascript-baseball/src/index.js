5501cbde8301c04a4e0f4cd15f038d0e9042be5a
publdaze <publdaze@naver.com>
2022-10-16 18:47:02 +0900

    feat: 볼, 스트라이크, 낫싱의 확인된 결과 출력

--- publdaze/javascript-baseball/src/index.js
+++ publdaze/javascript-baseball/src/index.js
@@ -31,6 +31,14 @@ const isOccuredError = inputNumArray => {
   return false;
 };
 
+const getOutputText = (strike, ball) => {
+  let output = '';
+  if (ball > 0) output += ball + '볼 ';
+  if (strike > 0) output += strike + '스트라이크';
+  if (output.length === 0) output = '낫싱';
+  return output;
+};
+
 export default function BaseballGame() {
   const { userInput, submit, gameRestartButton, result } = getElements();
 
@@ -39,14 +47,13 @@ export default function BaseballGame() {
 
     const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     const inputNumArray = userInput.value.split('').map(Number);
-    console.log(inputNumArray);
+    console.log(inputNumArray, randomNumbers);
     if (isOccuredError(inputNumArray)) userInput.value = '';
     else {
-      console.log(randomNumbers);
-      console.log(countMatchNumbersWithPos(randomNumbers, inputNumArray));
+      const strike = countMatchNumbersWithPos(randomNumbers, inputNumArray);
+      const ball = countMatchNumbers(randomNumbers, inputNumArray) - strike;
+      result.textContent = getOutputText(strike, ball);
     }
-
-    result.textContent = '';
   });
   gameRestartButton.addEventListener('click', function (e) {
     e.preventDefault();
