8d2b231f86b7656f5ef2f5de10761bd8a636e69f
publdaze <publdaze@naver.com>
2022-10-30 23:24:20 +0900

    feat: Car 인스턴스 배열 생성

--- publdaze/javascript-racingcar/src/index.js
+++ publdaze/javascript-racingcar/src/index.js
@@ -7,18 +7,26 @@ import {
 } from './modules/element.js';
 import { getInputNumber, getInputStringArray } from './modules/input.js';
 
+function Car(name) {
+  this.name = name;
+}
+
+const handleCarNames = (e) => {
+  e.preventDefault();
+
+  const carNames = getInputStringArray(carNamesInput, ',');
+  const cars = carNames.reduce((p, c) => [...p, new Car(c)], []);
+};
+
+const handleRacingCount = (e) => {
+  e.preventDefault();
+
+  const racingCount = getInputNumber(racingCountInput);
+};
+
 const Racing = () => {
-  carNamesSubmitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const carNames = getInputStringArray(carNamesInput, ',');
-  });
-
-  racingCountSubmitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const racingCount = getInputNumber(racingCountInput);
-  });
+  carNamesSubmitBtn.addEventListener('click', handleCarNames);
+  racingCountSubmitBtn.addEventListener('click', handleRacingCount);
 };
 
 Racing();
